2df637f7bff2237e085f6343a32e3570987fc635
daniesquivelc97 <dani_esquivelc@hotmail.com>
2022-05-04 12:33:03 -0500

    Diseño de tarjetas y pago

---
+++ lib/pages/tarjeta_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/credit_card_widget.dart';
+import 'package:stripe_app/models/tarjeta_credito.dart';
+import 'package:stripe_app/widgets/total_pay_button.dart';
+
+class TarjetaPage extends StatelessWidget {
+  const TarjetaPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final tarjeta = TarjetaCredito(
+      cardNumberHidden: '4242',
+      cardNumber: '4242424242424242',
+      brand: 'visa',
+      cvv: '213',
+      expiracyDate: '01/25',
+      cardHolderName: 'Daniel Esquivel',
+    );
+    print(tarjeta.cardNumber);
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Pagar'),
+          centerTitle: true,
+          backgroundColor: const Color(0xff284879),
+        ),
+        body: Stack(
+          children: [
+            Container(),
+            Hero(
+              tag: tarjeta.cardNumber,
+              child: CreditCardWidget(
+                cardNumber: tarjeta.cardNumberHidden,
+                expiryDate: tarjeta.expiracyDate,
+                cardHolderName: tarjeta.cardHolderName,
+                cvvCode: tarjeta.cvv,
+                isHolderNameVisible: true,
+                showBackView: false,
+                onCreditCardWidgetChange: (_) {},
+              ),
+            ),
+            const Positioned(
+              bottom: 0,
+              child: TotalPayButton(),
+            ),
+          ],
+        ));
+  }
+}
